8b20f0dede6e1f292655793d57e889291cffd6eb
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-01-17 17:05:49 -0500

    Finish 01 - 11 lessons

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // string to array
+    const array = string.split("");
+    // reverse array
+    const reverseArray = array.reverse();
+    // turn array into reverseString
+    const reverseString = reverseArray.join("")
+    return reverseString;
 };
 
 // Do not edit below this line
