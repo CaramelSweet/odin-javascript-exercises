ae83cc2bd4d366c59d4b651227a35ca2d600a855
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-02-27 06:47:17 -0500

    Finish Foundations Day 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const strip = word.replaceAll(/[\W_/]/g, "").toLowerCase();
+    const palindrome = strip.split("").reverse().join("");
+    return (strip == palindrome )? true : false;
 };
 
 // Do not edit below this line
