e646edf9630e8eab776cd336547b130301ed8142
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-03-01 02:15:54 -0500

    Finish 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) return("OOPS")
+    let a = 0;
+    let b = 1;
+    let temp;
+    for (let i = 0; i < num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return a;
 };
 
 // Do not edit below this line
