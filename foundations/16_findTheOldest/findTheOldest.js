304fbea17d839e8046311368f437bdf4ff9e0be2
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-03-01 03:23:13 -0500

    Finish foundations 16 findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+const currentYear = new Date().getFullYear();
+// const result = array.sort((a,b) => {
+//     const ageA = ((a.yearOfDeath || currentYear)  - a.yearOfBirth)
+//     const ageB = ((b.yearOfDeath || currentYear) - b.yearOfBirth)
+//     return ageB - ageA;
+// });
+// return result[0];
 
+return array.reduce((oldest, current) => {
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+    return currentAge > oldestAge ? current : oldest;
+});
 };
 
 // Do not edit below this line
